51cacbf80030c1829cdea38aabb255bf0cd50613
Phuong <phuong.dao@baruchmail.cuny.edu>
2024-11-24 20:07:28 -0500

    Changing step by working on the consent first

--- Cleaning data.R
+++ Cleaning data.R
@@ -29,6 +29,18 @@ pid_test <- dat |> filter(if_any(everything(), ~ grepl("test", .x, ignore.case=T
 '%!in%' <- function(x,y)!('%in%'(x,y))
 dat <- dat |> filter(pid %!in% pid_test)
 
+## Checking on duplicate consumers by looking at name
+dat |> select(consumer_name) |> mutate(consumer_name = tolower(str_trim(consumer_name))) |>
+  filter(consumer_name !="") |>
+  group_by(consumer_name) |> filter(n()>1) |> table()
+
+
+  group_by(consumer_name) |> filter(n()>1) |> select(consumer_name) |> table()
+  
+## Now we explore the duplicates individually 
+  dat |> filter(grepl("alexa roman", consumer_name, ignore.case=TRUE)) |> view() 
+## Alexa has two records both are done throughly 
+
 ## Correction the typo of pro_name
 ### Vidya Sharma has 19 different variations
 dat |> filter(grepl("sharma", pro_name, ignore.case=TRUE)) |> select(pid, pro_name)
@@ -50,11 +62,32 @@ dat |> filter(grepl("^(NA|N/A|none|idk)$", pro_name, ignore.case=TRUE)) |> selec
 
 # By manupulating all empty into NA, there would be 19 + 4 NA is 23 NA. Note that, in this case I use NA not "NA"
  dat <- dat|> mutate(pro_name=if_else(grepl("^(NA|N/A|none|idk)$", pro_name, ignore.case=TRUE),NA,pro_name))
+ 
+ ## Rodini
+dat <- dat |> mutate(pro_name = if_else(grepl("rodini", pro_name, ignore.case=TRUE),"Rodini Shiamilis", pro_name))
+   
+ 
+ dat <- dat |> mutate(pro_name = if_else(grepl("Mudunuru", pro_name, ignore.case=TRUE),"Mudunuru", pro_name))
+
+ ## white space cause a trouble for Spencer and Ryan  name
+ dat <- dat |> mutate(pro_name = if_else(grepl("spencer", pro_name, ignore.case=TRUE),"Spencer Washington", pro_name))
+ 
+dat <- dat |> mutate(pro_name = if_else(grepl("ryan", pro_name, ignore.case=TRUE),"Ryan Health", pro_name)) 
+
+levels(as.factor(dat$cbo_name))
+table(dat$consumer_name) # We see that ther are few names with more than 1 records
+dat |> group_by(consumer_name) |> filter(n() > 1) |> select(pid, consumer_name, pro_name, cbo_name, doi)
+levels(as.factor(dat$consumer_name))
 
  ## if want to show NA on the table we need to do 
  table(dat$pro_name, useNA = "ifany")
  
- ## After this step 
+ ## After correcting typo, now move to the factor part
+ ## By using levels function we can see if there is any typo in the values. It is a handy tool
+ 
+levels(dat$pro_name)
+levels(as.factor(dat$pro_name))
+
 ################################
 # ggplot(data=dat,mapping=aes(x=cbo_name, y=a1)+
 #          stat_summary(fun = "mean")
